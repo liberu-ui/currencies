f64b32fb524da7a20333bef3fd6117a4873cd057
mishu <mishu@xtelecom.ro>
2020-01-18 16:15:47 +0200

    adds a Conversion.js service and a vuex store module

---
+++ src/services/Conversion.js
@@ -0,0 +1,48 @@
+import route from '@enso-ui/ui/src/modules/plugins/route';
+
+class Conversion {
+    constructor() {
+        this.fromId = null;
+        this.toId = null;
+        this.amountToConvert = null;
+        this.conversionDate = null;
+    }
+
+    from(fromId) {
+        this.fromId = fromId;
+        return this;
+    }
+
+    to(toId) {
+        this.toId = toId;
+        return this;
+    }
+
+    amount(amountToConvert) {
+        this.amountToConvert = amountToConvert;
+        return this;
+    }
+
+    date(conversionDate) {
+        this.conversionDate = conversionDate;
+        return this;
+    }
+
+    handle() {
+        return axios.get(
+            route('administration.currencies.convert'),
+            { params: this.params() },
+        );
+    }
+
+    params() {
+        return {
+            from: this.fromId,
+            to: this.toId,
+            amount: this.amountToConvert,
+            date: this.conversionDate,
+        };
+    }
+}
+
+export default Conversion;
---
+++ src/store/currencies.js
@@ -0,0 +1,33 @@
+
+export const state = {
+    currencies: null,
+    countries: null,
+    defaultCurrency: null,
+};
+
+export const getters = {
+    currency: state => currencyId => state.currencies
+        .find(({ id }) => id === currencyId),
+    countryCurrency: state => (countryId) => {
+        const { currencyCode } = state.countries
+            .find(({ id }) => id === countryId);
+        return state.currencies
+            .find(({ code }) => code === currencyCode);
+    },
+};
+
+export const mutations = {
+    currencies: (state, currencies) => {
+        state.currencies = currencies;
+        state.defaultCurrency = currencies.find(currency => currency.isDefault);
+    },
+    countries: (state, countries) => (state.countries = countries),
+};
+
+
+export const actions = {
+    initialise({ commit }, { currencies, countries }) {
+        commit('currencies', currencies);
+        commit('countries', countries);
+    },
+};
